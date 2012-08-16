c4383eb470260caa71ab61acdf48d0efaeb5855e
Vorobyov <voronsupermen@gmail.com>
2012-08-16 18:13:23 +0400

    [[#3]] Создание приложения инициализирующего коллекцию элементов меню и отображающего контент страницы

---
+++ js/Views/accordionApp.js
@@ -0,0 +1,27 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Администратор
+ * Date: 14.08.12
+ * Time: 23:43
+ * To change this template use File | Settings | File Templates.
+ */
+app.Views.mainApp=Backbone.View.extend({
+
+        initialize: function(){
+
+        },
+        events: function(){
+
+        },
+        render :function(){
+
+        },
+        render_not_item : function(){
+
+        },
+        renderList: function(){
+
+        }
+
+
+});
