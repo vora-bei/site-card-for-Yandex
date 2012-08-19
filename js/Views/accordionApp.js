3eb2a4582adddab31c430210106b6ef9519a4a10
Mr Vorobyov <voronsupermen@gmail.com>
2012-08-19 21:19:45 +0400

    AccordionApp добавил открыть/закрыть все [[#7]] Заполнил информацией кроме пункта 1,6 [[#11]]

--- js/Views/accordionApp.js
+++ js/Views/accordionApp.js
@@ -8,26 +8,45 @@
 app.Views.accordionApp=Backbone.View.extend({
         head: 'h3',
         body:'div',
+        sections: '',
+        openBody : 0,
         initialize: function(options){
             this.parentApp=options.parentApp;
+            this.openBody= !_.isEmpty(options.openBody)?options.openBody:this.openBody;
             this.head=options.head?options.head:this.head;
             this.body=options.body?options.body:this.body;
             this.preRender()
         },
         events:{
+            'click .js-toggle': "accordionToggle"
+        },
+        accordionToggle: function(){
+            if(this.isAllOpen()){
+                this.sections.hide();
+            }else{
+                this.sections.show();
+            }
         },
         preRender :function(){
             this.events['click '+this.head]='toggle';
             this.delegateEvents();
-            this.$el.children(this.body).hide();
+            this.sections=this.$el
+                    .children(this.body)
+                        .hide()//скрыть все section
+            this.openByIndex(this.openBody)
+        },
+        openByIndex: function(index){
+            var section=this.sections.eq(index);
+                section?section.show():'';
+        },
+        isAllOpen:  function(){
+            return (this.sections.filter(':visible').length==this.sections.length)
         },
         toggle :function(events){
             var elem=this.$(events.currentTarget);
-            elem.next().toggle('slow',function(){})
-            return false;
+            elem.next().slideToggle('slow',function(){})
 
-        },
-        renderList: function(){
+            return false;
 
         }
 
